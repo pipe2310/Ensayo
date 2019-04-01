87cf2bd76e353aaf691549560c526e97cb617d7b
pipe2310 <48736018+pipe2310@users.noreply.github.com>
2019-03-31 22:12:50 -0400

    Update helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -508,9 +508,9 @@ var out = "";
 hbs.registerHelper('mostrarmatcursos2',()=>{
 let string ;
 var out = '<div class="accordion" id="accordionExample"> <div class="row">';
-listarAspp()
-listarMatt()
-listarr()
+listarAsp()
+listarMat()
+listar()
 i=1;
 let sw;
 listaCursos.forEach(cur=>{
@@ -575,9 +575,9 @@ i=i+1;
 hbs.registerHelper('mostrarmatcursos3',(identificador,documento)=>{
 let string ;
 var out = '<div class="accordion" id="accordionExample"> <div class="row">';
-listarAspp()
-listarMatt()
-listarr()
+listarAsp()
+listarMat()
+listar()
 i=1;
 let sw;
 listaCursos.forEach(cur=>{
